4206d975e7e961515109aef8bc78da6c0f02afd8
pieter <pieterhoekstra@gmail.com>
2024-07-23 02:39:59 +0200

    Remvoved console.log message

--- src/generate.js
+++ src/generate.js
@@ -2,6 +2,5 @@ import sudoku from './sudoku.js'
 
 addEventListener( 'message', ( msg ) => {
     let s = new sudoku.sudoku( msg.data.tiles )
-    console.log(s.getDifficulty())
     postMessage({tiles: s.generate(msg.data.d), d: s.getDifficulty()})
 })
